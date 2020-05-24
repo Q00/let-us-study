a6047ed6758a6359ce68009cef7a4fe5ccc6ff60
kyu <31264094+Q00@users.noreply.github.com>
2020-05-24 15:47:37 +0900

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+console.log('Try npm run check/fix!');
+
+const longString =
+  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ut aliquet diam.';
+
+// const trailing = 'Semicolon';
+
+// const why = 'am I tabbed?';
+
+console.log(longString);
+
+export function doSomeStuff(
+  withThis: string,
+  andThat: string,
+  andThose: string[],
+) {
+  //function on one line
+  if (!andThose.length) {
+    return false;
+  }
+  console.log(withThis);
+  console.log(andThat);
+  console.dir(andThose);
+  return;
+}
+// TODO: more examples
